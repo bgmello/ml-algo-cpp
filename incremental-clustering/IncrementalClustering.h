edf0cdafd6e04554fa7fe053ac91b298f018ab6e
Bruno Mello <bgorresenm@gmail.com>
2020-05-13 15:17:38 -0300

    clang format

--- incremental-clustering/IncrementalClustering.h
+++ incremental-clustering/IncrementalClustering.h
@@ -2,29 +2,25 @@
 #define __INCREMENTALCLUSTERING_H__
 #include <algorithm>
 #include <array>
-#include <set> 
+#include <set>
 #include <string>
 #include <vector>
 
-class IncrementalClustering
-{
+class IncrementalClustering {
+  double alpha;
+  int n;
 
-	double alpha;
-	int n;
+  std::set<std::string> get_ngram(std::string sentence);
 
-	std::set<std::string> get_ngram(std::string sentence);
+  double jaccard(std::set<std::string> ngram1, std::set<std::string> ngram2);
+  double distance(std::set<std::string> ngram1, std::set<std::string> ngram2);
 
-	double jaccard(std::set<std::string> ngram1, std::set<std::string> ngram2);
-	double distance(std::set<std::string> ngram1, std::set<std::string> ngram2);
+ public:
+  IncrementalClustering();
 
-	public:
+  IncrementalClustering(double alpha, int n);
 
-		IncrementalClustering();
-
-		IncrementalClustering(double alpha, int n);
-
-		std::vector<int> fit_predict(std::vector<std::string> sentences);
-
+  std::vector<int> fit_predict(std::vector<std::string> sentences);
 };
 
 #endif
--- incremental-clustering/main.cpp
+++ incremental-clustering/main.cpp
@@ -5,68 +5,62 @@
 
 #include "IncrementalClustering.h"
 
-int main(int argc, char* argv[])
-{
-	double alpha;
-	int n;
+int main(int argc, char *argv[]) {
+  double alpha;
+  int n;
 
-	std::string fileName;
-	std::vector<std::string> sentences;
-	std::ifstream inputFile;
-	std::ofstream outputFile;
+  std::string fileName;
+  std::vector<std::string> sentences;
+  std::ifstream inputFile;
+  std::ofstream outputFile;
 
-	if(argc==1)
-	{
-		std::cout << "No input file passed" << std::endl;
-		return 0;
-	}
+  if (argc == 1) {
+    std::cout << "No input file passed" << std::endl;
+    return 0;
+  }
 
-	else if(argc==2)
-	{
+  else if (argc == 2) {
+    std::cout << "No n passed, using 1" << std::endl;
+    n = 1;
+    std::cout << "No alpha passed, using 0.4" << std::endl;
+    alpha = 0.4;
 
-		std::cout << "No n passed, using 1" << std::endl;
-		n = 1;
-		std::cout << "No alpha passed, using 0.4" << std::endl;
-		alpha = 0.4;
+    fileName = argv[1];
+  }
 
-		fileName = argv[1];
-	}
+  else if (argc == 3) {
+    std::cout << "No alpha passed, using 0.4" << std::endl;
+    alpha = 0.4;
+    n = std::atoi(argv[1]);
+    fileName = argv[2];
+  }
 
-	else if(argc==3){
-		std::cout << "No alpha passed, using 0.4" << std::endl;
-		alpha = 0.4;
-		n = std::atoi(argv[1]);
-		fileName = argv[2];
-	}
+  else {
+    n = std::atoi(argv[1]);
+    alpha = std::atof(argv[2]);
+    fileName = argv[3];
+  }
 
-	else{
-		n = std::atoi(argv[1]);
-		alpha = std::atof(argv[2]);
-		fileName = argv[3];
-	}
+  inputFile.open(fileName);
+  outputFile.open(fileName + ".output");
 
-	inputFile.open(fileName);
-	outputFile.open(fileName+".output");
+  std::string line;
 
-	std::string line;
+  while (std::getline(inputFile, line)) {
+    sentences.push_back(line);
+  }
 
-	while(std::getline(inputFile, line))
-	{
-		sentences.push_back(line);
-	}
+  IncrementalClustering inc(alpha, n);
 
-	IncrementalClustering inc(alpha, n);
+  std::vector<int> clusters = inc.fit_predict(sentences);
 
-	std::vector<int> clusters = inc.fit_predict(sentences);
+  for (int i = 0; i < clusters.size(); i++) {
+    outputFile << sentences[i] << '\t' << clusters[i];
+    outputFile << std::endl;
+  }
 
-	for(int i=0;i<clusters.size();i++)
-	{
-		outputFile << sentences[i] << '\t' << clusters[i];
-		outputFile << std::endl;
-	}
+  inputFile.close();
+  outputFile.close();
 
-	inputFile.close();
-	outputFile.close();
-	
-	return 0;
+  return 0;
 }
